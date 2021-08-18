b5811bd4f1b36bf242406e02d9e000c8e320ebda
tom_glb <goodMorning_glb@atguigu.com>
2021-08-18 21:54:49 +0800

    安装路由

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+//该文件专门用于创建整个应用的路由器
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({  
+    history: createWebHistory(),  
+    routes:[
+    ]
+})
+
+export default router
