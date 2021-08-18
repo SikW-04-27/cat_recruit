730ff411a39ae2785547e3e846a42641cd9fa2f7
linhaorui <2909753663@qq.com>
2021-08-18 20:39:04 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
- 
+ <Register/>
 </template>
 
 <script>
 
+import Register from './views/register/Register.vue'
 
 export default {
   name: "App",
   components: {
-  
+  Register
   },
 };
 </script>
