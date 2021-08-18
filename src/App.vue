77d3cad8c20e657f019bec87578b6e0a80efb450
linhaorui <2909753663@qq.com>
2021-08-18 15:35:32 +0800

    新增文件夹

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  哈哈哈哈哈哈哈哈哈11111122222333334444555555
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  1316516515631
+ 
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+  
   },
 };
 </script>
