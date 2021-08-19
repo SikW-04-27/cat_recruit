bb523c829e76274d189fdd6c260b3b35a27853e4
tom_glb <goodMorning_glb@atguigu.com>
2021-08-19 21:40:21 +0800

    更新管理端

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,6 @@ import Register from './views/register/Register.vue'
 import UserNav from './components/UserNav.vue'
 import Manage from './views/manage/manage.vue'
 
-
-
 export default {
   name: "App",
   components: {
