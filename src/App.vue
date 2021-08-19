ce7c8c1b25f56ab606a92772229c03d2e696e605
haber <1907685975@qq.com>
2021-08-20 00:45:34 +0800

    8.19号提交

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-
-
+  <Appointment></Appointment>
 </template>
 
 <script>
-
-import Register from './views/register/Register.vue'
-import UserNav from './components/UserNav.vue'
-
+import Register from "./views/register/Register.vue";
+import UserNav from "./components/UserNav.vue";
+import Appointment from "./views/appointment/Appointment.vue";
 
 export default {
   name: "App",
   components: {
-  Register,
-  UserNav
-  }
-}
+    Register,
+    UserNav,
+    Appointment,
+  },
+};
 </script>
---
+++ src/views/signup/select.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="i_select">
+    <el-select v-model="value" filterable placeholder="请选择所在学院">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: "Select",
+  setup() {
+    let options = ref([
+      {
+        value: "选项1",
+        label: "计算机学院",
+      },
+      {
+        value: "选项2",
+        label: "自动化学院",
+      },
+      {
+        value: "选项3",
+        label: "机械学院",
+      },
+      {
+        value: "选项4",
+        label: "数学学院",
+      },
+      {
+        value: "选项5",
+        label: "其他",
+      },
+    ]);
+    return {
+      options,
+      value: ref(""),
+    };
+  },
+};
+</script>
+
+<style>
+</style>
