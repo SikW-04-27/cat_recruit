e6c75c4b26badad8b1b442579e10498a99ef312c
haber <1907685975@qq.com>
2021-09-02 21:25:37 +0800

    9.2

---
+++ .history/src/request/http_20210901104959.js
@@ -0,0 +1,80 @@
+//引入axios
+import axios from 'axios';
+// import { response } from 'express';
+// 引入qs模块，用来序列化post类型的数据
+// import qs from 'qs'
+// import resolve from 'resolve';
+
+// 环境的切换
+if (process.env.NOOE_ENV == 'development') {
+    axios.defaults.baseURL = '/api';
+} else if (process.env.NOOE_ENV == 'debug') {
+    axios.defaults.baseURL = '';
+} else if (process.env.NOOE_ENV == 'production') {
+    axios.defaults.NOOE_ENV = '';
+}
+
+// 设置请求超时
+axios.defaults.timeout = 10000;
+
+axios.defaults.headers.get['Content-Type'] = 'application/json';
+
+// post请求头的设置
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+
+// 请求拦截?????????????????????????????????????????????????????????????????????????????????????????????????????????
+axios.interceptors.request.use(
+    config => {
+        config.headers
+        let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzA0NjYxNzcsInV1aWQiOiJzV2pEQ05BQSJ9.b5W4hneOfbJThKQDPGV7zkgOA1_Y2NqGQ8qJWEPYwW0";
+        config.headers.accessToken = token;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+)
+
+// 响应拦截?????????????????????????????????????????????????????????????????????????????????????????????????????????
+axios.interceptors.response.use(
+    response => {
+        if (response.status === 200) {
+            return Promise.resolve(response);
+        } else {
+            return Promise.reject(response);
+        }
+    },
+    error => {
+        return Promise.reject(error.response);
+    }
+)
+
+// 封装get方法
+export function get(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+            params: params
+        }).then(res => {
+            console.log("请求成功");
+            resolve(res.data);
+        }).catch(err => {
+            console.log("请求失败");
+            reject(err);
+        })
+    })
+}
+
+// 封装post请求
+export function post(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.post(url, params)
+            .then(res => {
+                console.log("请求成功");
+                resolve(res.data);
+            }).catch(err => {
+                console.log("请求失败");
+                reject(err);
+            })
+    })
+}
+
---
+++ .history/src/views/appointment/Appointment_20210901223253.vue
@@ -0,0 +1,260 @@
+<template>
+  <div id="appointment">
+    <Tips v-if="tipStatus"> </Tips>
+    <!-- 页眉 -->
+    <router-link to="/introduction">
+      <el-page-header @back="goBack" content="预约面试时间"> </el-page-header>
+    </router-link>
+    <el-divider>目前预约：{{ data }}</el-divider>
+
+    <!-- 预约 -->
+    <div class="a_content" v-if="true">
+      <span>请选择预约时间：</span>
+      <!-- 预约日期 -->
+      <el-select
+        v-model="value"
+        filterable
+        placeholder="请选择预约日期"
+        class="el-select"
+        :disabled="disabled"
+        @change="options_Change($event)"
+      >
+        <el-option
+          v-for="item in day"
+          :key="item.id"
+          :label="item.totalTime"
+          :value="item.totalTime"
+        >
+        </el-option>
+      </el-select>
+
+      <!-- （提示）已预约 -->
+      <div class="tips text-center" v-if="!signupStatus">
+        <el-result icon="success" class="float-left"></el-result>
+        <span class="float-left">预约成功，预约时间为：{{ value }}</span>
+      </div>
+
+      <!-- 预约按钮 -->
+      <div class="a_btn">
+        <el-button
+          type="primary"
+          round
+          v-if="!signupStatus"
+          @click="comfirm"
+          class="c_btn"
+          >确定预约</el-button
+        >
+        <el-button type="danger" round v-if="signupStatus" @click="cancel"
+          >取消预约</el-button
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, onMounted } from "vue";
+import { ElMessage } from "element-plus";
+import "tailwindcss/tailwind.css";
+import {
+  listAppointment,
+  getCurrentStatus,
+  getUserStatus,
+  updateUserInfo,
+  cancelAppointment,
+} from "../../request/api.js";
+import Select from "../signup/select.vue";
+import Tips from "./Tips.vue";
+export default {
+  name: "Appointment",
+  components: {
+    Tips,
+  },
+  setup() {
+    // 当前所处时段
+    let data = ref("");
+
+    // 预约日期
+    let day = reactive([]);
+    //控制tips框是否显示
+    let tipStatus = ref(false);
+    // 选择框的值
+    let value = ref("");
+    //判断是否已预约
+    let signupStatus = ref(false);
+    // 控制选择框禁止状态
+    let disabled = ref(false);
+    //设置选项的key值（即时间的id值）
+    let key = ref();
+    //点击返回按钮
+    const goBack = () => {
+      console.log("go back");
+    };
+    let warningMessage = ref("");
+    //定义提示函数：
+    const warning = () => {
+      ElMessage.warning({
+        message: warningMessage,
+        type: "warning",
+      });
+    };
+    //当选择框里面的值改变时：(传递的参数就是选择框里的值)
+    let options_Change = ($event) => {
+      [...day].filter((item) => {
+        if (item.totalTime === $event) {
+          key = item.id;
+        }
+      });
+    };
+
+    // 查看全部预约时间(调接口)
+    listAppointment({}).then((res) => {
+      for (var i = 0; i < res.data.length; i++) {
+        res.data[i].totalTime =
+          res.data[i].beginTime + " ~ " + res.data[i].endTime;
+      }
+      day.push(...res.data);
+    });
+
+    //查看目前总体招新状态
+    getCurrentStatus({}).then((res) => {
+      console.log(res.data.status);
+      data.value = res.data.status;
+    });
+
+    //comfirm函数，点击预约时调用
+    let comfirm = () => {
+      // 当选择框内有内容时
+      if (value.value != "") {
+        updateUserInfo({
+          id: "fTbQodpa",
+          appointmentId: key,
+        })
+          .then((res) => {
+            signupStatus.value = !signupStatus.value; //切换预约状态
+            disabled.value = !disabled.value; //切换选择框是否禁选
+            console.log(res);
+          })
+          .catch((err) => {
+            warningMessage = err.data.message;
+            warning();
+          });
+        //预约成功后查看用户的状态
+        // getUserStatus({}).then((res) => {
+        //   console.log(res);
+        // });
+      }
+      // 当选择框没内容时
+      else {
+        warningMessage = "请选择预约时间";
+        warning();
+      }
+    };
+
+    //cancel取消函数，点击取消预约
+    let cancel = () => {
+      signupStatus.value = !signupStatus.value;
+      value.value = "";
+      disabled.value = !disabled.value;
+      //取消预约
+      cancelAppointment({
+        id: "fTbQodpa",
+      }).then((res) => {
+        console.log(res);
+      });
+    };
+
+    return {
+      data,
+      day,
+      goBack,
+      value,
+      signupStatus,
+      tipStatus,
+      comfirm,
+      cancel,
+      disabled,
+      key,
+      options_Change,
+      warning,
+      warningMessage,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#appointment {
+  width: 80%;
+  margin: 40px auto;
+  padding: 0 50px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+  text-align: center;
+  position: relative;
+  background-color: rgba(78, 78, 78, 0.5);
+  color: azure;
+  border-radius: 10px;
+}
+.el-result {
+  ::v-deep .icon-success {
+    width: 30px;
+    height: auto;
+  }
+}
+
+.el-page-header {
+  padding: 30px 0 0 0;
+}
+.el-divider {
+  margin-bottom: 80px;
+}
+.a_content {
+  height: 250px;
+}
+span {
+  color: rgb(216, 216, 216);
+}
+.el-select {
+  display: inline-block;
+  margin: 0 20px;
+  width: 350px;
+}
+.el-input__inner {
+  text-align: center;
+}
+.el-result {
+  display: inline-block;
+  padding: 0;
+  width: 50px;
+}
+.el-result svg {
+  width: 30px;
+  height: 30px;
+}
+.el-page-header__left {
+  color: rgb(216, 216, 216);
+}
+::v-deep.el-page-header__content {
+  color: rgb(216, 216, 216);
+}
+.a_btn {
+  margin-top: 50px;
+  .el-button {
+    background-color: #2f86b6;
+    border: #ffffff00;
+    border-radius: 5px;
+  }
+}
+.c_btn {
+  margin-top: 50px;
+}
+.tips {
+  height: 50px;
+  padding: 30px 0px;
+  text-align: center;
+  span{
+    line-height: 30px;
+    display: inline-block;
+  }
+}
+</style>
