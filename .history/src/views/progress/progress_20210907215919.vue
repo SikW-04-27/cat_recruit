19734d0167466d253718acfbcd2d1d60760ea794
haber <1907685975@qq.com>
2021-09-08 21:34:00 +0800

    大致完成

---
+++ .history/src/views/progress/progress_20210907215919.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="progress">
+    <router-link to="/introduction">
+      <el-page-header @back="goBack" content="面试进度"> </el-page-header>
+    </router-link>
+    <div class="block" v-if="allowing && stuId">
+      <el-timeline>
+        <!-- <el-timeline-item
+          v-for="(activity, index) in activities"
+          :key="index"
+          :icon="activity.icon"
+          :type="activity.type"
+          :color="activity.color"
+          :size="activity.size"
+          :timestamp="activity.timestamp"
+        > -->
+        <el-timeline-item v-for="(activity, index) in activities" :key="index">
+          <el-alert :title="activity.status" type="info" :closable="false">
+          </el-alert>
+          <!-- {{ activity.content }} -->
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+    <!-- 当无法查询时的页面 -->
+    <div class="close" v-if="!allowing && stuId">{{ closeMessage }}</div>
+  </div>
+  <!-- 未登陆时 -->
+  <div class="unLogin" v-if="!stuId">
+    <span>您还未登录，请先登录</span>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, defineComponent, onMounted } from "vue";
+import { getBriefInfo } from "../../request/api";
+import _ from "lodash";
+//点击返回按钮
+const goBack = () => {
+  console.log("go back");
+};
+//检测用户是否能查询
+let allowing = ref(true);
+let closeMessage = ref("无法加载");
+let stuId = window.localStorage.getItem("userId");
+let activities = reactive([]);
+onMounted(() => {
+  getBriefInfo({}).then(res=>{
+    console.log(res);
+  })
+  //获取所有阶段
+  // getAllStatus({})
+  //   .then((res) => {
+  //     console.log(res);
+  //     activities.push(...res.data);
+  //     //调用查询接口
+  //     getUserStatus({})
+  //       .then((res) => {
+  //         console.log(res);
+  //         console.log(activities[0]);
+  //         //处理用户经历过的阶段
+  //         for (let i = 0; i < activities.length; i++) {
+  //           if (activities[i].status === res.data.recruitmentStatus) {
+  //             let n = i;
+  //             console.log(n);
+  //             activities.splice(n + 1, activities.length);
+  //             console.log(activities);
+  //           }
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         allowing.value = false;
+  //         if (err.data.message) {
+  //           closeMessage.value = err.data.message;
+  //         }
+  //       });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+});
+</script>
+
+<style scoped lang="scss">
+#progress {
+  width: 80%;
+  margin: 0 auto;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+  // background-color: #fff;
+}
+.el-page-header {
+  padding: 30px;
+  color: #727272;
+}
+.block,
+.close {
+  padding: 0 60px;
+}
+.close {
+  width: 100%;
+  height: 300px;
+  padding: 100px;
+  color: aliceblue;
+  text-align: center;
+}
+.rank {
+  margin: 0px 100px 30px;
+  display: inline-block;
+  color: #606266;
+}
+.unLogin {
+  text-align: center;
+  padding: 100px 0;
+  color: #fff;
+}
+</style>
---
+++ .history/src/views/signIn/signIn_20210905222313.vue
@@ -0,0 +1,261 @@
+<template>
+  <div class="signIn">
+    <!-- 页眉 -->
+    <router-link to="/introduction">
+      <el-page-header @back="goBack" content="面试签到"> </el-page-header>
+    </router-link>
+    <el-divider>{{ data }}</el-divider>
+
+    <!-- 当签到开放 && 用户未签到 -->
+    <div class="s_btn" v-if="opening && !UserCheck && stuId">
+      <!-- 点击签到的按钮 -->
+      <el-button
+        type="primary"
+        round
+        v-if="!signupStatus"
+        @click="comfirm"
+        class="c_btn"
+        >点击签到</el-button
+      >
+      <!-- 显示签到成功的信息 -->
+      <div class="tips text-center" v-if="signupStatus">
+        <el-result icon="success" class="float-left"></el-result>
+        <span class="float-left">签到成功，你的排位是{{ value }}</span>
+      </div>
+    </div>
+
+    <!-- 当签到开放 && 用户已签到 -->
+    <div class="hasCheck" v-if="opening && UserCheck && stuId">
+      <el-result icon="success"></el-result>
+      <span>您已签到，你的排位是{{ value }}，请等待叫号</span>
+    </div>
+
+    <!-- 签到未开放时 或者用户未报名 或用户未签到 -->
+    <div class="close" v-if="!opening && stuId">
+      <el-result icon="danger"></el-result>
+      <span>{{closeMessage}}</span>
+    </div>
+
+    <!-- 当用户未登录 -->
+    <div class="unLogin" v-if="!stuId">
+      <span>您还未登录，请先登录</span>
+    </div>
+
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+import { ElMessage } from "element-plus";
+import {
+  queue,
+  queueStatus,
+  checkUserStatus,
+  getUserStatus,
+  cancelSignIn,
+  listWaitingUser,
+} from "../../request/api";
+//是否开放签到
+let opening = ref(false);
+//标题显示签到状态
+let data = ref("未开放签到");
+//用户是否已签到
+let UserCheck = ref(false);
+//是否点击签到
+let signupStatus = ref(false);
+//签到后的排号
+let value = ref("*");
+//消息提示
+let message = ref("");
+//其他情况下显示的提示
+let closeMessage = ref("");
+//warning:
+let warningMessage = ref("");
+let stuId = window.localStorage.getItem("userId");
+console.log(stuId);
+const warning = () => {
+  ElMessage.warning({
+    message: warningMessage,
+    type: "warning",
+  });
+};
+
+const goBack = () => {
+  console.log("goBack");
+};
+onMounted(() => {
+  //若无登录：
+  if (!stuId) {
+    return;
+  }
+  //若处于前两个阶段
+  if(window.localStorage.getItem("CurrentStatus")){
+
+  }
+  //1.获取当前签到是否开启
+  queueStatus({}).then((res) => {
+    console.log(res);
+    if (res.code === 1101) {
+      data.value = res.message;
+      opening.value = true;
+    } else {
+      //未处于签到状态
+      opening.value = false;
+      closeMessage.value = "未开放签到，请耐心等候";
+    }
+  });
+  //获取用户是否已报名
+  getUserStatus({})
+    .then((res) => {
+      if (res.data.userStatus === "已报名" || "未预约") {
+      } else {
+        opening.value = false;
+        closeMessage.value = "用户未报名，请先报名";
+      }
+    })
+    .catch((err) => {});
+  //是否预约
+  getUserStatus({})
+    .then((res) => {
+      if (res.data.userStatus == "等待") {
+        //已经预约的情况
+      } else {
+        //未预约
+        opening.value = false;
+        closeMessage.value = "用户未预约，请先预约";
+      }
+    })
+    .catch((err) => {
+      warningMessage = err.data.message;
+      warning();
+    });
+
+  //2.检测用户是否已经签到
+  checkUserStatus({ id: stuId })
+    .then((res) => {
+      console.log(res);
+      UserCheck.value = true;
+      //获取用户排名
+      listWaitingUser({})
+        .then((res) => {
+          console.log(res);
+          for (var i = 0; i < res.data.length; i++) {
+            if (res.data[i].id === stuId) {
+              value.value = ref(i + 1);
+            }
+          }
+          if (value.value === "*") {
+            value.value === "已叫号";
+          }
+        })
+        .catch((err) => {
+          warningMessage.value = err.data.message;
+          warning();
+        });
+    })
+    .catch((err) => {
+      warningMessage.value = err.data.message;
+      warning();
+    });
+
+  //3.用户未签到时，点击签到按钮
+  let comfirm = () => {
+    queue({})
+      .then((res) => {
+        console.log(res);
+        signupStatus.value = !signupStatus.value;
+      })
+      .catch((err) => {
+        console.log(err.data.message);
+        warningMessage.value = err.data.message;
+        warning();
+      });
+    //获取用户的排位(等接口ing)
+    listWaitingUser({})
+      .then((res) => {
+        console.log(res);
+        for (var i = 0; i < res.data.length; i++) {
+          if (res.data[i].id === stuId) {
+            value.value = ref(i + 1);
+          }
+        }
+        if (value.value === "*") {
+          value.value === "已叫号";
+        }
+      })
+      .catch((err) => {
+        warningMessage.value = err.data.message;
+        warning();
+      });
+  };
+  //4，取消签到（不在页面展示该功能）
+  // cancelSignIn({})
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+});
+</script>
+
+<style lang="scss" scoped>
+.signIn {
+  background-color: #fff;
+  margin: 40px auto 0;
+  width: 80%;
+  padding: 20px 50px 0 50px;
+}
+.s_btn {
+  margin-top: 100px;
+  padding-bottom: 50px;
+  text-align: center;
+  .c_btn {
+    display: block;
+    margin: 0 auto;
+  }
+  .tips {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+    span {
+      line-height: 30px;
+    }
+  }
+}
+.close,
+.hasCheck {
+  display: block;
+  text-align: center;
+  padding: 50px 0;
+  span {
+    line-height: 30px;
+    display: inline-block;
+    vertical-align: middle;
+  }
+}
+.el-result {
+  vertical-align: middle;
+  :deep .icon-success {
+    display: block;
+    width: 30px;
+    height: 30px;
+  }
+  :deep .icon-info {
+    display: block;
+    width: 30px;
+    height: 30px;
+  }
+}
+.tips,
+.close {
+  .el-result {
+    padding: 0;
+    margin-right: 20px;
+    display: inline-block;
+  }
+}
+.unLogin {
+  text-align: center;
+  padding: 100px 0;
+}
+</style>
